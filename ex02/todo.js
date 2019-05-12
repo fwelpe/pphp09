ace56408236906a1670d217e69afc7e776714e57
Crawford Denys arryn <cdenys-a@ox-d4.21-school.ru>
2019-05-12 13:37:18 +0300

    final

--- ex02/todo.js
+++ ex02/todo.js
@@ -6,7 +6,8 @@ parent_div = document.getElementById("ft_list");
 
 const add_new = () => {
     let newText = prompt("new TO DO: ");
-    setElem(newText, -1);
+    if (newText)
+        setElem(newText, -1);
 }
 
 setElem = (newText, idFromCookies) => {
@@ -15,34 +16,37 @@ setElem = (newText, idFromCookies) => {
     newElem.setAttribute("id", idFromCookies === -1 ? i : idFromCookies);
     newElem.appendChild(document.createTextNode(newText));
     parent_div.insertBefore(newElem, parent_div.childNodes[0]);
-    if (idFromCookies === -1)
-    document.cookie = `${i}=${newText}`;
-    i += 1;
+    if (idFromCookies === -1) {
+        var date = new Date(new Date().getTime() + 24 * 3600 * 1000);
+        document.cookie = `${i}=${newText}; expires=` + date.toUTCString();
+        i += 1;
+    }
 }
 
-const deleteCookie = (cname) => {
+/* const deleteCookie = (cname) => {
     var d = new Date();
     d.setTime(d.getTime() - (1000*60*60*24));
     var expires = "expires=" + d.toGMTString();
     window.document.cookie = cname+"="+"; "+expires;
-}
+} */
 
 const removeElem = (w) => {
     if (confirm("Confirm removing TO DO")) {
         let id = w.getAttribute("id");
         w.parentNode.removeChild(w);
-        deleteCookie(id);
+        document.cookie = `${id}=; expires=-1;`;
+        // deleteCookie(id);
     }
 }
 
 const cookStr = document.cookie;
 const cookArr = cookStr.split('; ');
-const mapCook = (q) => {
+const addCook = (q) => {
     const ck = q.split('=');
-    if (ck[1]) {
+    if (ck[1] && !isNaN(ck[0])) {
         setElem(ck[1], ck[0]);
-        if (Number(ck[0]) > i)
-            i = Number(ck[0]) + 1;
+        if (Number(ck[0]) >= i)
+            i = 1 + Number(ck[0]);
     }
 }
-const cookObj = cookArr.map(mapCook);
+cookArr.forEach(addCook);
